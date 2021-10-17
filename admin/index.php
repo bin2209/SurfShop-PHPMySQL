18a238b5d84a18b8b1159906a240b6c9e24628e3
binazure2299 <58648810+binazure2299@users.noreply.github.com>
2021-10-17 15:02:17 +0700

    Fix store in admin | error $where_category

--- admin/index.php
+++ admin/index.php
@@ -2,6 +2,8 @@
 include('includes/header.php');
 require_once("../core/db_conn.php");
 require_once("../classes/Functions.php");
+
+
 session_start();
 if(!isset($_SESSION['user_id']) && (!isset($_SESSION['type']))){
 header('Location: ../login.php');
