357be854148d45bef093711a9a4d4a775fe6a7e2
binazure2299 <58648810+binazure2299@users.noreply.github.com>
2021-11-17 22:55:49 +0700

    new them cart
    new them cart

--- assets/js/total-price.js
+++ assets/js/total-price.js
@@ -1,72 +1,119 @@
-sum_price=0;
-quantity=0;
-document.getElementById("total-price-result").innerHTML = sum_price;
-document.getElementById("quantity-result").innerHTML = quantity;
-function PriceFormat(price){
-	price = Intl.NumberFormat('vi-VN', { maximumSignificantDigits: 3 }).format(price);
-	return price;
-}
-function isSelectAll(){
-	// CHECK XEM NẾU TẤT CẢ ĐÃ SELECTED
-	checkboxes = document.getElementsByName('product');
-	for(var i=0, n=checkboxes.length;i<n;i++) {
-		if (checkboxes[i].checked==false){
-			return 0;
-		}
-	}
-	return 1;
-}
-function isnotSelectAll(){
-	// CHECK XEM NẾU TẤT CẢ KHÔNG CÓ CÁI NÀO SELECT CẢ
-	checkboxes = document.getElementsByName('product');
-	for(var i=0, n=checkboxes.length;i<n;i++) {
-		if (checkboxes[i].checked==true){
-			return 0;
-		}
-	}
-	return 1;
+sum_price = 0;
+quantity = 0;
+
+function Print_Checkout(quantity, sum_price) {
+    if (quantity == 0) {
+        document.getElementById("checkout-content").innerHTML = '<p style="font-size: 18px; font-weight: 500;">' + LANG_choose_product + '</p>';
+    } else {
+        if (quantity == 1) {
+            LANG_VIPproduct = LANG_product;
+        } else {
+            LANG_VIPproduct = LANG_products;
+        }
+        document.getElementById("checkout-content").innerHTML = '<div> <span>' + quantity + ' ' + LANG_VIPproduct + ' </span><br> <span>' + LANG_price + ': </span><span style="font-weight:600;">' + PriceFormat(sum_price) + '₫</span> <br> <span>' + LANG_Delivery_charges + ': </span><span style="font-weight:600;">' + LANG_Delivery_free + '</span><br> <hr> <span style="font-weight:600;">' + LANG_total_pay + ': </span><span style="font-weight:600;">' + PriceFormat(sum_price) + '₫</span><br> (' + LANG_Tax_included + ') <button class="checkout-button" style="width:100%; margin-top:10px;">' + LANG_checkout + '</button> </div>';
+    }
 }
 
-function selectOne(source){
-	if (isSelectAll()==true){
-		document.getElementById("selectAllbutton").checked=true;
-	}else{
-		document.getElementById("selectAllbutton").checked=false;
-	}
-	if (source.checked==true){
-		sum_price = Number(sum_price)+Number(source.value);
-		quantity= Number(quantity)+1;
-	}else if (source.checked==false){
-		sum_price = Number(sum_price)-Number(source.value);
-		quantity= Number(quantity)-1;
-
-	}
-	sum_price_format = PriceFormat(sum_price);
-	document.getElementById("quantity-result").innerHTML = quantity;
-	document.getElementById("total-price-result").innerHTML = sum_price_format;
+function PriceFormat(price) {
+    price = Intl.NumberFormat('vi-VN', { maximumSignificantDigits: 3 }).format(price);
+    return price;
 }
 
-function selectAll(source){
-	checkboxes = document.getElementsByName('product');
-	for(var i=0, n=checkboxes.length;i<n;i++) {
-		
-		if (checkboxes[i].checked==false){
-			sum_price =  Number(sum_price)+ Number(checkboxes[i].value);
-			checkboxes[i].checked = source.checked;
-		}
-
-	}
-	if (isnotSelectAll()==true){
-		sum_price = 0;
-		quantity = 0;
-	}
-	if (isSelectAll()==true){
-		quantity = Number(n);
-		document.getElementById("selectAllbutton").checked=true;
-	}
-	sum_price_format = PriceFormat(sum_price);
-	document.getElementById("quantity-result").innerHTML = quantity;
-	document.getElementById("total-price-result").innerHTML = sum_price_format;
+function isSelectAll() {
+    // CHECK XEM NẾU TẤT CẢ ĐÃ SELECTED
+    checkboxes = document.getElementsByName('product');
+    for (var i = 0, n = checkboxes.length; i < n; i++) {
+        if (checkboxes[i].checked == false) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
+function isnotSelectAll() {
+    // CHECK XEM NẾU TẤT CẢ KHÔNG CÓ CÁI NÀO SELECT CẢ
+    checkboxes = document.getElementsByName('product');
+    for (var i = 0, n = checkboxes.length; i < n; i++) {
+        if (checkboxes[i].checked == true) {
+            return 0;
+        }
+    }
+    return 1;
+}
 
+function selectOne(source) {
+    if (isSelectAll() == true) {
+        document.getElementById("selectAllbutton").checked = true;
+    } else {
+        document.getElementById("selectAllbutton").checked = false;
+    }
+    if (source.checked == true) {
+        sum_price = Number(sum_price) + Number(source.value);
+        quantity = Number(quantity) + 1;
+    } else if (source.checked == false) {
+        sum_price = Number(sum_price) - Number(source.value);
+        quantity = Number(quantity) - 1;
+    }
+    sum_price_format = PriceFormat(sum_price);
+    document.getElementById("quantity-result").innerHTML = quantity;
+    document.getElementById("total-price-result").innerHTML = sum_price_format;
+}
+
+function selectAll(source) {
+    checkboxes = document.getElementsByName('product');
+    for (var i = 0, n = checkboxes.length; i < n; i++) {
+
+        if (checkboxes[i].checked == false) {
+            sum_price = Number(sum_price) + Number(checkboxes[i].value);
+            checkboxes[i].checked = source.checked;
+        }
+    }
+    if (isnotSelectAll() == true) {
+        sum_price = 0;
+        quantity = 0;
+    }
+    if (isSelectAll() == true) {
+        quantity = Number(n);
+        document.getElementById("selectAllbutton").checked = true;
+    }
+    sum_price_format = PriceFormat(sum_price);
+    document.getElementById("quantity-result").innerHTML = quantity;
+    document.getElementById("total-price-result").innerHTML = sum_price_format;
+}
+
+$(document).ready(function() {
+    if ($.cookie('language') == 'en') {
+        LANG_choose_product = 'Please choose a product.';
+        LANG_product = 'item';
+        LANG_products = 'items';
+        LANG_price = 'Total Delivery Cost';
+        LANG_Delivery_charges = 'Total Delivery Cost';
+        LANG_Delivery_free = 'Free';
+        LANG_total_pay = 'Total';
+        LANG_Tax_included = 'Inclusive of tax';
+        LANG_checkout = 'Check out';
+    } else {
+        LANG_choose_product = 'Vui lòng chọn sản phẩm.';
+        LANG_product = 'mặt hàng';
+        LANG_products = 'mặt hàng';
+        LANG_price = 'Tổng Giá Trị Sản Phẩm';
+        LANG_Delivery_charges = 'Phí giao hàng';
+        LANG_Delivery_free = 'Miễn phí';
+        LANG_total_pay = 'Tổng cộng';
+        LANG_Tax_included = 'Đã bao gồm thuế';
+        LANG_checkout = 'Thanh toán';
+    }
+    document.getElementById("total-price-result").innerHTML = sum_price;
+    document.getElementById("quantity-result").innerHTML = quantity;
+    $("#checkout").click(function() {
+        Print_Checkout(quantity, sum_price);
+        $('.checkout').addClass('open');
+        $('.content-center').addClass('blur-filter');
+        $('footer').addClass('blur-filter');
+    });
+    $('.pop-up .content .close').click(function() {
+        $('.pop-up').removeClass('open');
+        $('.content-center').removeClass('blur-filter');
+        $('footer').removeClass('blur-filter');
+    });
+});
--- templates/cart.php
+++ templates/cart.php
@@ -44,11 +44,9 @@ if ($stmt->rowCount() === 1) {
         </div>
         <div id="order-products" class="row">
             <div class="col-12 grid-margin">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table">
-                                <?php
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <?php
 									$total_price = 0;
 									if ($bag_item==''){
 										echo '
@@ -56,61 +54,51 @@ if ($stmt->rowCount() === 1) {
 										<br>
 										<span class="order-empty">'.$LANG_bag_empty.'</span>';
 									}else{
-										$bag_item = explode(',',$bag_item);
+                                        $bag_item = explode(',',$bag_item);
 										$bag_item_id = explode(',',$bag_item_id);
 										$i = 0;
-										echo '<thead>
-										<tr>
-										<th style="text-align: left;">'.$LANG_product.'</th>
-										<th>'.$LANG_unit_price.'</th>
-										<th>'.$LANG_quantity.'</th>
-										<th>'.$LANG_total.'</th>
-										<th>'.$LANG_manipulation.'</th>
-										</tr>
-										</thead>
-
-										<tbody>';
-										foreach ($bag_item as $item) {
-
+                                        foreach ($bag_item as $item) {
 											$stmt = $conn->prepare("SELECT * FROM store WHERE id=?");
-
 											$stmt->execute(array($item));
 											if ($stmt->rowCount() === 1) {
 												$item = $stmt->fetch();
 												$item_id = $item["id"];
 												$item_images = $item["images"];
+                                                $item_type = $item["type"];
 												$item_name = $item["name"];
 												$item_price = $item["price"];
 												$item_brand = $item["brand"];
 												$total_price += $item["price"];
 											}
+                                            echo ' 
+                                            <div class="Cart-product">
+                                            <div class="left-content">
+                                                <input onclick="selectOne(this)" type="checkbox" name="product"
+                                                    value="'.$item_price.'">
+                                                <a href="../store/product/'.$item_id.'">
+                                                    <img src="../uploads/products/'.$item_images.'">
+                                                </a>
+                                            </div>
+                                            <div class="right-content">
+                                                <div class="Erow">
+                                                   <i onclick="remove_cart(this.id)" class="fas fa-times"></i>
+                                                </div>
+                                                <div class="Crow">
+                                                    <span> '.$item_type.'| '.$item_name.'</span>
+                                                    <span>'.$item_brand.'</span>
+                                                    <span class="price">'.s_PriceFormat($item_price).' ₫</span>
+                                                    <input type="number" value="1">
+                                                </div>
+                                            </div>
+                                        </div>';
+                                        }
+                                    }
+                                    ?>
 
-											echo '<tr>';
+                        <style>
+                      
+                        </style>
 
-											echo '<td>
-											<span class="pl-2"><input onclick="selectOne(this)" type="checkbox" name="product" value="'.$item_price.'"></span>
-
-											<img src="../uploads/products/'.$item_images.'"/>
-											<div>
-
-											<span class="pl-2">'.$item_name.'</span>
-											<span class="pl-2">'.$item_brand.'</span>
-											</div>
-											</td>
-											<td>'.s_PriceFormat($item_price).'₫</td>
-											<td>1</td>
-											<td>'.s_PriceFormat($item_price).'₫</td>
-											<td>
-											<button id="'.$bag_item_id[$i].'" class="remove-button" onclick="remove_cart(this.id)"  >'.$LANG_remove.'</button>
-											</td>';
-											echo '</tr>';
-											$i++;
-										}
-									}
-									?>
-                                </tbody>
-                            </table>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -121,13 +109,39 @@ if ($stmt->rowCount() === 1) {
                     style="position:relative; display: inline;"> <?php echo $LANG_selectall ?> </span>
             <span class="total-payment-span"><?php echo $LANG_payment ?>: <p id="total-price-result"
                     style="display: inline;"></p> ₫</span>
-            <span class="checkout-span"><button id="" class="checkout-button"><?php echo $LANG_checkout ?> (<p
+            <span class="checkout-span"><button id="checkout" class="checkout-button"><?php echo $LANG_checkout ?> (<p
                         id="quantity-result" style="display: inline;"></p>)</button></span>
         </div>
     </div> <!--  // backgound content -->
 </section>
 </body>
 </div>
+
+<div class="pop-up checkout">
+    <div class="content">
+        <div class="container">
+            <span class="close">close</span>
+            <div class="title">
+                <h1><?=$LANG_checkout;?></h1>
+            </div>
+            <div id="checkout-content" class="checkout-content">
+            </div>
+        </div>
+    </div>
+</div>
+<style>
+.pop-up .content .container {
+    padding: 2em;
+}
+
+.pop-up {
+    min-width: 350px;
+}
+
+.checkout-content {
+    text-align: left;
+}
+</style>
 <script type="text/javascript">
 function remove_cart($id) {
     $.post("<?php echo $_DOMAIN;?>/request/removefrombag.php", {
@@ -153,5 +167,4 @@ function remove_cart($id) {
 <script type="text/javascript" src="<?php echo $_DOMAIN;?>/assets/js/total-product-ispined.js"></script>
 
 </html>
-
 <?php include('includes/footer.php'); ?>
